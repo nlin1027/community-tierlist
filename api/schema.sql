bce5bb747e2de2d240434293b371d348d8ce9cf4
nlin1027 <nathan.lin.chungking@gmail.com>
2026-07-03 19:29:34 -0400

    updated postgres table to include timestamp

--- api/schema.sql
+++ api/schema.sql
@@ -2,8 +2,12 @@ CREATE TABLE IF NOT EXISTS deadlock (
   id SERIAL PRIMARY KEY,
   "user" TEXT NOT NULL,
   character TEXT NOT NULL,
-  tier CHAR(1) NOT NULL CHECK (tier IN ('D', 'C', 'B', 'A', 'S', 'Z'))
+  tier CHAR(1) NOT NULL CHECK (tier IN ('D', 'C', 'B', 'A', 'S', 'Z')),
+  date TIMESTAMP DEFAULT NOW()
 );
 
 --@block
 SELECT * FROM deadlock;
+
+--@block
+DROP TABLE IF EXISTS deadlock;
